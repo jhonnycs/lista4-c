e5c5db87110190628b5397344c116e5b7540284e
jhonnycs <87949563+jhonnycs@users.noreply.github.com>
2023-11-06 11:51:48 -0300

    suposta questão 15 terminada

--- questao15.c
+++ questao15.c
@@ -18,13 +18,21 @@ int main() {
     } else {
         printf("A palavra não é palíndromo");
     }
-
-        return 0;
+    return 0;
 }
 
 int verificarPalindromo(char *palavra) {
     char palavra1[ARRAY_SIZE], palavra2[ARRAY_SIZE];
+    int tamanhoPalavra = sizeof(palavra);
 
-    for (int i=0; i<ARRAY_SIZE; i++) {
+    for (int i=0; i<tamanhoPalavra; i++) {
+        palavra1[i] = palavra[i];
+        palavra2[i] = palavra[tamanhoPalavra-i-1];
+    }
+
+    if (palavra1==palavra2) {
+        return 1;
+    } else {
+        return 0;
     }
 }
