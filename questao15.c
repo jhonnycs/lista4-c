5cc69d3026f5156b68e683646f348edb7c53f82c
jhonnycs <87949563+jhonnycs@users.noreply.github.com>
2023-11-06 11:34:06 -0300

    Update questao15.c

--- questao15.c
+++ questao15.c
@@ -6,9 +6,25 @@
 */
 
 #define ARRAY_SIZE 30
+int verificarPalindromo(char *palavra);
 
 int main() {
     char palavra[ARRAY_SIZE];
+    printf("Digite uma palavra:\n> ");
+    scanf("%s", palavra);
+
+    if (verificarPalindromo(palavra)) {
+        printf("A palavra é palíndromo");
+    } else {
+        printf("A palavra não é palíndromo");
+    }
 
         return 0;
+}
+
+int verificarPalindromo(char *palavra) {
+    char palavra1[ARRAY_SIZE], palavra2[ARRAY_SIZE];
+
+    for (int i=0; i<ARRAY_SIZE; i++) {
+    }
 }
