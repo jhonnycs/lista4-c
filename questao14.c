c9baeb94dd8a6e63cd459fb8da963cd7d473b7dc
jhonnycs <jhonnycunha7@gmail.com>
2023-11-06 01:52:37 -0300

    questão 14 incompleta

--- questao14.c
+++ questao14.c
@@ -14,14 +14,10 @@
             2.3 ocorre 1 vezes
 
 */
-int estaNoArray(float num, float *vetor, int tamanho);
-void popularArray(float num, float *array, int tamanho);
 
 int main() {
     int quantValores;
     float sequencia[quantValores];
-    float sequenciaSemRepeticao[quantValores];
-    int totalOcorrencias[quantValores];
 
     printf("Digite o número de dígitos das sequências de números:\n> ");
     scanf("%d", &quantValores);
@@ -34,10 +30,7 @@ int main() {
     for (int i = 0; i < quantValores; i++) {
         printf("Digite o número %d:\n> ", i + 1);
         scanf("%f", &sequencia[i]);
-        totalOcorrencias[i] = -1;
     }
-
-    popularArray(M_PI, sequenciaSemRepeticao, quantValores);
 
     for (int i = 0; i < quantValores; i++) {
         int quantOcorrencias = 0;
@@ -48,41 +41,9 @@ int main() {
             }
         }
 
-        if (!estaNoArray(sequencia[i], sequencia, quantValores)) {
-            for (int j = 0; j < quantValores; j++) {
-                if (sequenciaSemRepeticao[j] == (float)M_PI) {
-                    sequenciaSemRepeticao[j] = sequencia[i];
-                }
-                if (totalOcorrencias[j] == -1) {
-                    totalOcorrencias[j] = quantOcorrencias;
-                    break;
-                }
-            }
-        }
+        printf("\nO número %.2f ocorre %d vezes", sequencia[i],
+               quantOcorrencias);
     }
 
-    for (int i = 0; i < quantValores; i++) {
-        printf("%.2f %d", sequenciaSemRepeticao[i], totalOcorrencias[i]);
-        if (sequenciaSemRepeticao[i] != M_PI && totalOcorrencias[i] != -1) {
-            printf("\nO número %.2f ocorre %d vezes", sequencia[i],
-                   totalOcorrencias[i]);
-        } else {
-            break;
-        }
-    }
-
-    return 0;
-}
-
-void popularArray(float num, float *array, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        array[i] = num;
-    }
-}
-
-int estaNoArray(float num, float *vetor, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        if (num == vetor[i]) return 1;
-    }
     return 0;
 }
